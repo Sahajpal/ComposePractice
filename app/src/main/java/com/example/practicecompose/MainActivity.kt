c277d6c7d65d80adb949329a6326f60f2608329b
Sahaj Pal Singh Mahla <54036040+Sahajpal@users.noreply.github.com>
2023-09-29 22:29:01 +0530

    feat: Added Greeting Section and Chip Section

--- app/src/main/java/com/example/practicecompose/MainActivity.kt
+++ app/src/main/java/com/example/practicecompose/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.practicecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.practicecompose.ui.HomeScreen
 import com.example.practicecompose.ui.theme.PracticeComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PracticeComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Sahaj")
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PracticeComposeTheme {
-        Greeting("Android")
     }
 }
