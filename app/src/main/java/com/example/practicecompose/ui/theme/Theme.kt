87b2c75ec056455a361021c21e0d180f2d6f3968
Sahaj Pal Singh Mahla <54036040+Sahajpal@users.noreply.github.com>
2023-10-03 19:12:08 +0530

    style: Made some Font and theme changes

--- app/src/main/java/com/example/practicecompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/practicecompose/ui/theme/Theme.kt
@@ -7,33 +7,12 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-
-)
-
-private val LightColorPalette = lightColors(
-
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
-
 @Composable
-fun PracticeComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun PracticeComposeTheme(
+    content: @Composable() () -> Unit
+) {
 
     MaterialTheme(
-        colors = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
