dc8a39e6680fbb877ddbb047ba2e396b05af1779
Sahaj Pal Singh Mahla <54036040+Sahajpal@users.noreply.github.com>
2023-10-03 19:14:37 +0530

    chore: Cleaned and completed the Meditation UI

--- app/src/main/java/com/example/practicecompose/Feature.kt
+++ app/src/main/java/com/example/practicecompose/Feature.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.graphics.Color
 data class Feature(
     val title: String,
     @DrawableRes val iconId: Int,
-    val lightColor : Color,
-    val mediumColor : Color,
-    val darkColor : Color,
+    val lightColor: Color,
+    val mediumColor: Color,
+    val darkColor: Color,
 )
